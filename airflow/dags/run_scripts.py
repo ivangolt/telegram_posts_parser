18effed7a855d27f93038b70624aa9ad625ade5f
ivan golt <102701882+ivangolt@users.noreply.github.com>
2024-10-28 23:24:59 +0300

    upd: update airflow pipeline

--- airflow/dags/run_scripts.py
+++ airflow/dags/run_scripts.py
@@ -9,29 +9,13 @@ from airflow.operators.python_operator import PythonOperator
 logger = logging.getLogger("airflow.task")
 
 
-# Define functions to run each script with logging
-def run_load_dataset():
-    logger.info("Starting load_dataset task")
-    try:
-        subprocess.run(["python", "/path/to/load_dataset.py"], check=True)
-        logger.info("Successfully completed load_dataset task")
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Error in load_dataset task: {e}")
-
-
-def run_transform_dataset():
-    logger.info("Starting transform_dataset task")
-    try:
-        subprocess.run(["python", "/path/to/transform_dataset.py"], check=True)
-        logger.info("Successfully completed transform_dataset task")
-    except subprocess.CalledProcessError as e:
-        logger.error(f"Error in transform_dataset task: {e}")
-
-
 def run_upload_dataset_to_s3():
     logger.info("Starting upload_dataset_to_s3 task")
     try:
-        subprocess.run(["python", "/path/to/upload_dataset_to_s3.py"], check=True)
+        subprocess.run(
+            ["python", "C:/dev/telegram_posts_parser/scripts/push_to_storage.py"],
+            check=True,
+        )
         logger.info("Successfully completed upload_dataset_to_s3 task")
     except subprocess.CalledProcessError as e:
         logger.error(f"Error in upload_dataset_to_s3 task: {e}")
@@ -54,17 +38,9 @@ with DAG(
     catchup=False,
 ) as dag:
     # Define tasks
-    load_dataset_task = PythonOperator(
-        task_id="load_dataset", python_callable=run_load_dataset
-    )
-
-    transform_dataset_task = PythonOperator(
-        task_id="transform_dataset", python_callable=run_transform_dataset
-    )
 
     upload_dataset_task = PythonOperator(
         task_id="upload_dataset_to_s3", python_callable=run_upload_dataset_to_s3
     )
 
-    # Set task dependencies
-    load_dataset_task >> transform_dataset_task >> upload_dataset_task
+    upload_dataset_task
